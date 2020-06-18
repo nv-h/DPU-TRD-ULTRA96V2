ff6ffe1f8f989bf8f8aa6e025978b81059d8b077
nv-h <saido.nv@gmail.com>
2020-06-18 15:24:13 +0900

    環境変数にsdk.shで展開した環境設定を追加、DNNDKのサンプルを追加 (切りが悪いが、これからサンプルを変更するので一旦コミット)

--- setting_env.sh
+++ setting_env.sh
@@ -1,3 +1,6 @@
 export VITIS_AI_HOME=~/sources/Vitis-AI
 export SDX_PLATFORM=~/sources/Avnet/vitis/platform_repo/ULTRA96V2/ULTRA96V2.xpfm
-export TRD_HOME=$(pwd)/DPU-TRD-ULTRA96V2
+export TRD_HOME=$(pwd)
+
+unset LD_LIBRARY_PATH
+source ~/work/petalinux_sdk_vai_1_1_dnndk/environment-setup-aarch64-xilinx-linux
