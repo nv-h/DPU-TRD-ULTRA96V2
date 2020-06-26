f5966ebcee76d1e6a1b33a5cfe81f3883d212fa8
nv-h <saido.nv@gmail.com>
2020-06-26 22:14:02 +0900

    ultra96v2_oob_2019_2.bspでプロジェクト作成

---
+++ prj/petalinux/ultra96v2_oob_2019_2_ai/hardware/ULTRA96V2_2019_2/ULTRA96V2.srcs/sources_1/bd/ULTRA96V2/ip/ULTRA96V2_blk_mem_gen_0_0/ULTRA96V2_blk_mem_gen_0_0_stub.vhdl
@@ -0,0 +1,45 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
+-- Date        : Fri Jun 26 20:03:19 2020
+-- Host        : saido-Lin running 64-bit Ubuntu 18.04.4 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/saido/sources/Avnet/hdl/Projects/ultra96v2_oob/ULTRA96V2_2019_2/ULTRA96V2.srcs/sources_1/bd/ULTRA96V2/ip/ULTRA96V2_blk_mem_gen_0_0/ULTRA96V2_blk_mem_gen_0_0_stub.vhdl
+-- Design      : ULTRA96V2_blk_mem_gen_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xczu3eg-sbva484-1-e
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity ULTRA96V2_blk_mem_gen_0_0 is
+  Port ( 
+    clka : in STD_LOGIC;
+    rsta : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    addra : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 127 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 127 downto 0 );
+    clkb : in STD_LOGIC;
+    rstb : in STD_LOGIC;
+    enb : in STD_LOGIC;
+    web : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    addrb : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    dinb : in STD_LOGIC_VECTOR ( 127 downto 0 );
+    doutb : out STD_LOGIC_VECTOR ( 127 downto 0 );
+    rsta_busy : out STD_LOGIC;
+    rstb_busy : out STD_LOGIC
+  );
+
+end ULTRA96V2_blk_mem_gen_0_0;
+
+architecture stub of ULTRA96V2_blk_mem_gen_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,rsta,ena,wea[15:0],addra[31:0],dina[127:0],douta[127:0],clkb,rstb,enb,web[15:0],addrb[31:0],dinb[127:0],doutb[127:0],rsta_busy,rstb_busy";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_4,Vivado 2019.2";
+begin
+end;
---
+++ prj/petalinux/ultra96v2_oob_2019_2_ai/hardware/ULTRA96V2_2019_2/ULTRA96V2.srcs/sources_1/bd/ULTRA96V2/ip/ULTRA96V2_clk_wiz_0_0/ULTRA96V2_clk_wiz_0_0_stub.vhdl
@@ -0,0 +1,37 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
+-- Date        : Fri Jun 26 20:04:40 2020
+-- Host        : saido-Lin running 64-bit Ubuntu 18.04.4 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/saido/sources/Avnet/hdl/Projects/ultra96v2_oob/ULTRA96V2_2019_2/ULTRA96V2.srcs/sources_1/bd/ULTRA96V2/ip/ULTRA96V2_clk_wiz_0_0/ULTRA96V2_clk_wiz_0_0_stub.vhdl
+-- Design      : ULTRA96V2_clk_wiz_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xczu3eg-sbva484-1-e
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity ULTRA96V2_clk_wiz_0_0 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_out2 : out STD_LOGIC;
+    clk_out3 : out STD_LOGIC;
+    clk_out4 : out STD_LOGIC;
+    clk_out5 : out STD_LOGIC;
+    clk_out6 : out STD_LOGIC;
+    clk_out7 : out STD_LOGIC;
+    resetn : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end ULTRA96V2_clk_wiz_0_0;
+
+architecture stub of ULTRA96V2_clk_wiz_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_out2,clk_out3,clk_out4,clk_out5,clk_out6,clk_out7,resetn,locked,clk_in1";
+begin
+end;
